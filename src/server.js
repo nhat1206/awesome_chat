32cd2543ee1fce4811eaa4d4575d5ad2e00d2d19
nhat1206 <nhnhat1206@gmail.com>
2021-11-05 21:01:35 +0700

    add routes and controllers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
+import initRoutes from "./routes/web";
 require("dotenv").config();
 let app = express();
 
@@ -10,13 +11,8 @@ ConnectDB();
 //Config view engine
 configViewEngine(app);
 
-app.get("/",(req,res)=>{
-   res.render("main/master");
-});
-
-app.get("/loginRegister",(req,res)=>{
-    return res.render("auth/loginRegister");
-});
+//Init all routes
+initRoutes(app);
 
 app.listen(process.env.APP_PORT,process.env.APP_HOSTNAME,()=>{
     console.log(`running at ${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`);
