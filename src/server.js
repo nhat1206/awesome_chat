db2bb8f6169e3615d5602f368aaba93bb9d35bcc
nhat1206 <nhnhat1206@gmail.com>
2021-11-05 16:46:59 +0700

    staic page

--- src/server.js
+++ src/server.js
@@ -1,23 +1,21 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
 require("dotenv").config();
 let app = express();
 
 //Connect to mongodb
 ConnectDB();
 
-app.get("/test-database",async (req,res)=>{
-    try {
-        let item = {
-            userId: "qwe123",
-            contactId: "sgsgf2424",
-        }
-        let contact = await ContactModel.createNew(item)
-        res.send(contact);
-    } catch (error) {
-        console.log(err);
-    }
+//Config view engine
+configViewEngine(app);
+
+app.get("/",(req,res)=>{
+   res.render("main/master");
+});
+
+app.get("/loginRegister",(req,res)=>{
+    return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT,process.env.APP_HOSTNAME,()=>{
