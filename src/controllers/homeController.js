80178bfe493b976858928d8d4166652a4382b50d
nhat1206 <nhnhat1206@gmail.com>
2021-11-15 01:08:33 +0700

    render contact

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,4 @@
-import {notification} from "./../services/index";
+import {notification,contact} from "./../services/index";
 
 let getHome = async (req,res)=>{
 
@@ -8,12 +8,34 @@ let getHome = async (req,res)=>{
     //get amount notification unread
     let countNotifUnread = await notification.countNotifUnread(req.user._id); 
 
+    //get contacts 10 item per time
+    let contacts = await contact.getContacts(req.user._id);
+
+    //get contact sent 10 item per time
+    let contactsSent = await contact.getContactsSent(req.user._id);
+
+    //get contact received 10 item per time
+    let contactsReceived = await contact.getContactsReceived(req.user._id);
+
+    //count contacts
+    let countAllContacts = await contact.countAllContacts(req.user._id);
+
+    let countAllContactsSent = await contact.countAllContactsSent(req.user._id);
+
+    let countAllContactsReceived = await contact.countAllContactsReceived(req.user._id);
+
     return res.render("main/home/home",{
-    errors: req.flash("errors"),
-    success: req.flash("success"),
-    user: req.user,
-    notifications: notifications,
-    countNotifUnread: countNotifUnread
+        errors: req.flash("errors"),
+        success: req.flash("success"),
+        user: req.user,
+        notifications: notifications,
+        countNotifUnread: countNotifUnread,
+        contacts: contacts,
+        contactsSent: contactsSent,
+        contactsReceived:contactsReceived,
+        countAllContacts: countAllContacts,
+        countAllContactsSent: countAllContactsSent,
+        countAllContactsReceived: countAllContactsReceived
     });
 };
 
--- src/sockets/contact/addNewContact.js
+++ src/sockets/contact/addNewContact.js
@@ -8,7 +8,8 @@ let addNewContact = (io) =>{
             let currentUser = {
                 id: socket.request.user._id,
                 username: socket.request.user.username,
-                avatar: socket.request.user.avatar
+                avatar: socket.request.user.avatar,
+                address: (socket.request.user.address !== null) ? socket.request.user.address : "" 
             };
             if (clients[data.contactId]) {
                 emitNotifyToArray(clients,data.contactId,io,"response-add-new-contact",currentUser);
